0e1ad01c4a2d2a94e54331904c1e52b9aef7455c
VoynovDmitry <cry_miss@mail.ru>
2015-09-23 21:22:39 +0300

    commit

--- laba2_2.cpp
+++ laba2_2.cpp
@@ -4,21 +4,40 @@
 
 using namespace std;
 
-double pi(double epsilon)
+float realPi = 3.14159265358979323846264338327950288419716939937510;
+
+float pi(float epsilon)
 {
-	const double PI = 3.14159265358979323846;
-	double pi = 0;
+	const float PI = 3.14159265358979323846;
+	float pi = 0;
 	for (int k = 0; ; k++)
 	{
-		pi += 4*pow(-1, k)/(2*k+1);
+		if (k%2 == 0) 
+			pi += 4/(2*(float)k+1);
+		else
+			pi -= 4/(2*(float)k+1);
 		if (abs(PI - pi) <= epsilon) break;
 	}
 	return pi;
 }
 
+void assert (float tochnost)
+{
+	if (abs(realPi - pi(tochnost))<=tochnost)
+		cout << "OK" << endl;
+	else 
+		cout << "FAIL" << endl;
+}
+
 int main()
 {
-	
+	assert(0.1);
+	assert(0.01);
+	assert(0.001);
+	assert(0.0001);
+	assert(0.0000002);
+	assert(0.0000000000029);
 	return 0;
 }
+
 
