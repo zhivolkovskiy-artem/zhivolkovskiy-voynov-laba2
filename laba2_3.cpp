5bc6277a00ef5650fea761504cee188135cfc542
zhivolkovskiy-artem <heartkilla66@gmail.com>
2015-09-24 11:55:11 +0300

    third commit

--- laba2_3.cpp
+++ laba2_3.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "stdafx.h"
 
 using namespace std;
+
+void assert(int number, int test)
+{
+	int result = имяфункции(number);
+	if (test == result)
+	{
+		cout << "OK" << endl;
+	}
+	else
+	{
+		cout << "FAIL: " << number << endl;
+	}
+}
 
 int numberOfZeros(int x)
 {
@@ -15,7 +29,16 @@ int numberOfZeros(int x)
 
 int main()
 {
-
+	assert(0, 0);
+	assert(1, 0);
+	assert(12, 2);
+	assert(25, 6);
+	assert(9, 1);
+	assert(16, 3);
+	assert(30, 7);
+	assert(255, 63);
+	assert(666, 165);
+	assert(54404, 13598);
 	return 0;
 
 }
